8ff007e419bec22e3833ecc39d514de19005cb76
shenprabu <54752317+shenprabu@users.noreply.github.com>
2024-06-13 03:10:57 +0530

    reusing cswXMLJSONConverter instance

--- src/com/parser/ConverterFactory.java
+++ src/com/parser/ConverterFactory.java
@@ -4,7 +4,12 @@ import com.parser.interfaces.XMLJSONConverter;
 
 public class ConverterFactory {
 
+    XMLJSONConverter cswXMLJSONConverter = null;
+
     public static XMLJSONConverter getConverter() {
-        return new CSWXMLJSONConverter();
+        if(this.cswXMLJSONConverter == null) {
+            this.cswXMLJSONConverter = new CSWXMLJSONConverter();
+        }
+        return this.cswXMLJSONConverter;
     }
 }
